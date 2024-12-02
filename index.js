84d0507fe6574eeee418f78197f7f9a4a2a6e6b7
RoyBao96 <baoruyi.hc07@gmail.com>
2024-12-01 23:25:28 -0800

    a6 4.2

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import session from "express-session";
+import mongoose from "mongoose";
 import "dotenv/config";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 
 app.use(cors({credentials: true,
